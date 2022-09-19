d3121682c652c7cfd48b551f43963edd70735fd3
Sombre-all <111535816+Sombre-all@users.noreply.github.com>
2022-09-19 13:59:41 +0000

    Update config.js

--- config.js
+++ config.js
@@ -41,26 +41,26 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['761192103'] //ur owner number
-global.ownername = "Udavin Wijesundara"
+global.owner = ['2250596531751'] //ur owner number
+global.ownername = "𝙎𝙊𝙈𝘽𝙍𝙀 𝘿𝘼𝙀𝙈𝙊𝙉"
 global.ytname = "YT: UW TECH SHOW" //ur yt chanel name
 global.socialm = "Insta: udavin_wijesundara" //ur github or insta name
 global.location = "Sri-Lanka" //ur location
 
 //bot bombdy
-global.botname = "White King WhatsApp Bot" //ur bot name
+global.botname = "𝙎𝙊𝙈𝘽𝙍𝙀 𝘾𝙃𝙀𝙀𝙈𝙎" //ur bot name
 global.websitex = "https://www.youtube.com/channel/UC8CNAgNIXy1O2vLBBSN7tcA" //ur website
 global.vidmenu = { url: 'https://a.uguu.se/zlzQZvXH.mp4' } //gif and video menu
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n🦄🦄🦄 "
+global.packname = "𝙎𝙊𝙈𝘽𝙍𝙀 𝘿𝘼𝙀𝙈𝙊𝙉 "
 global.author = "Udavin Wijesundara"
-global.themeemoji = "🐶"
-global.reactmoji = "😘"
-global.ownertag = ['761192103'] //ur owner tag
-global.ownernummenu = ['761192103'] //ur owner number in menu and all
-global.watermark = "White King WhatsApp Bot." //ur watermark
+global.themeemoji = "🕊️"
+global.reactmoji = "🪄"
+global.ownertag = ['2250596531751'] //ur owner tag
+global.ownernummenu = ['2250596531751'] //ur owner number in menu and all
+global.watermark = "𝙎𝙊𝙈𝘽𝙍𝙀 𝘾𝙃𝙀𝙀𝙈𝙎." //ur watermark
 global.botscript = "https://www.youtube.com/watch?v=vXBO1zJbGJ4"
-global.linkz1 = "https://chat.whatsapp.com/Ivsw8fEw5lW0n0qKEDHfUX"
-global.linkz2 = "https://chat.whatsapp.com/Jub3moTE7jaBERo8DqWXC1"
+global.linkz1 = "https://chat.whatsapp.com/H1qJWRPdraODl4TVOwf7Yy"
+global.linkz2 = "https://chat.whatsapp.com/H1qJWRPdraODl4TVOwf7Yy"
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
